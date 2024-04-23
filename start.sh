727d430224e744b99dbc4b5af87e90ab9d99d077
Jerry <yoyo@openmbox.net>
2024-04-23 23:38:06 +0800

    优化启动脚本,忽略已存在的环境变量

--- start.sh
+++ start.sh
@@ -6,6 +6,14 @@ npx prisma migrate deploy
 export VERSION=$(cat /app/version)
 
 ENV_FILE="/app/.env"
-export $(grep -v '^#' "$ENV_FILE" | grep -v '^$' | xargs)
+
+# 读取 .env 文件并导出环境变量，跳过已存在的环境变量
+grep -v -e '^#' -e '^$' "$ENV_FILE" | while read -r line; do
+    key=$(echo "$line" | cut -d '=' -f 1)
+    if [ -z "${!key+x}" ]; then
+        # 导出整个键值对
+        export "$line"
+    fi
+done
 
 node /app/.output/server/index.mjs > output.log 2>&1
