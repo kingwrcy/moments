760fad57d1d64e07c3f8e2f7e41bb3195711588f
dinphy <dinphy1205@126.com>
2024-06-06 17:51:57 +0800

    UI 细节优化：调整评论、点赞工具栏样式

--- components/FriendsMemo.vue
+++ components/FriendsMemo.vue
@@ -65,22 +65,22 @@
           class="toolbar-icon px-2 py-1 bg-[#f7f7f7] dark:bg-slate-700 hover:bg-[#dedede] cursor-pointer rounded flex items-center justify-center">
           <img src="~/assets/img/dian.svg" class="w-3 h-3" />
         </div>
-        <div class="text-xs absolute top-[-8px] right-[30px] bg-[#4c4c4c] rounded text-white p-2" v-if="showToolbar"
+        <div class="absolute top-[-8px] right-[32px] bg-[#4c4c4c] rounded text-white p-2 px-4" v-if="showToolbar"
           ref="toolbarRef">
           <div class="flex flex-row gap-4">
-            <div class="flex flex-row gap-2 cursor-pointer items-center" v-if="token"
+            <div class="flex flex-row gap-1 cursor-pointer items-center" v-if="token"
               @click="pinned(); showToolbar = false">
               <Pin :size=14 />
               <div class="hidden md:block">{{ (props.memo.pinned ? '取消' : '') + '置顶' }}</div>
             </div>
-            <div class="flex flex-row gap-2 cursor-pointer items-center"
+            <div class="flex flex-row gap-1 cursor-pointer items-center"
               v-if="token && !route.path.startsWith('/detail')" @click="editMemo">
               <FilePenLine :size=14 />
               <div class="hidden md:block">编辑</div>
             </div>
             <AlertDialog>
               <AlertDialogTrigger asChild>
-                <div class="flex flex-row gap-2 cursor-pointer items-center" v-if="token">
+                <div class="flex flex-row gap-1 cursor-pointer items-center" v-if="token">
                   <Trash2 :size=14 />
                   <div class="hidden md:block">删除</div>
                 </div>
@@ -98,18 +98,17 @@
                 </AlertDialogFooter>
               </AlertDialogContent>
             </AlertDialog>
-
-            <div class="flex flex-row gap-2 cursor-pointer items-center" @click="like">
+            <span class="bg-[#6b7280] h-[20px] w-[1px] block md:hidden" v-if="token"></span>
+            <div class="flex flex-row gap-1 cursor-pointer items-center" @click="like">
               <Heart :size=14 v-if="likeList.findIndex((id) => id === props.memo.id) < 0" />
               <HeartCrack :size=14 v-else />
-              <div class="hidden md:block">{{ likeList.findIndex((id) => id === props.memo.id) >= 0 ? '取消' : '赞' }}
-              </div>
+              <div>{{ likeList.findIndex((id) => id === props.memo.id) >= 0 ? '取消' : '赞' }}</div>
             </div>
-
-            <div class="flex flex-row gap-2 cursor-pointer items-center" v-if="publicConfig.enableComment"
+            <span class="bg-[#6b7280] h-[20px] w-[1px]" v-if="!token"></span>
+            <div class="flex flex-row gap-1 cursor-pointer items-center" v-if="publicConfig.enableComment"
               @click="momentsShowCommentInput = !momentsShowCommentInput; showUserCommentArray = []; showToolbar = false">
               <MessageSquareMore :size=14 />
-              <div class="hidden md:block">评论</div>
+              <div>评论</div>
             </div>
           </div>
         </div>
