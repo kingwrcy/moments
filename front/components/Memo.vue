6d6dbd48fa31c8186445f5d011cedecc3e748cf6
DIANSO <huoyanlei@gmail.com>
2025-03-11 18:19:12 +0800

    实现在首页点击memo正文进入详情页
    代码由windsurf生成

--- front/components/Memo.vue
+++ front/components/Memo.vue
@@ -48,20 +48,15 @@
         </div>
 
         <div class="flex flex-col gap-2">
-          <div :class="{'cursor-pointer': !isDetailPage}" @click="!isDetailPage && navigateTo(`/memo/${item.id}`)">
-            <external-url-preview :favicon="item.externalFavicon" :title="item.externalTitle" :url="item.externalUrl"
-                                  v-if="item.externalFavicon&&item.externalTitle&&item.externalUrl"/>
-          </div>
-          <div>
-            <upload-image-preview :imgs="item.imgs" :imgConfigs="item.imgConfigs"/>
-          </div>
-          <div :class="{'cursor-pointer': !isDetailPage}" @click="!isDetailPage && navigateTo(`/memo/${item.id}`)">
-            <music-preview v-if="extJSON.music && extJSON.music.id" v-bind="extJSON.music"/>
-            <douban-book-preview v-if="extJSON.doubanBook && extJSON.doubanBook.title" :book="extJSON.doubanBook"/>
-            <douban-movie-preview v-if="extJSON.doubanMovie && extJSON.doubanMovie.title" :movie="extJSON.doubanMovie"/>
-            <video-preview-iframe v-if="extJSON.video && ['bilibili', 'youtube'].includes(extJSON.video.type) && extJSON.video.value" :url="extJSON.video.value"/>
-            <video-preview v-if="extJSON.video && extJSON.video.type === 'online' && extJSON.video.value" :url="extJSON.video.value"/>
-          </div>
+          <external-url-preview :favicon="item.externalFavicon" :title="item.externalTitle" :url="item.externalUrl"
+                                v-if="item.externalFavicon&&item.externalTitle&&item.externalUrl"/>
+          <upload-image-preview :imgs="item.imgs" :imgConfigs="item.imgConfigs" :memo-id="item.id"/>
+
+          <music-preview v-if="extJSON.music && extJSON.music.id" v-bind="extJSON.music"/>
+          <douban-book-preview v-if="extJSON.doubanBook && extJSON.doubanBook.title" :book="extJSON.doubanBook"/>
+          <douban-movie-preview v-if="extJSON.doubanMovie && extJSON.doubanMovie.title" :movie="extJSON.doubanMovie"/>
+          <video-preview-iframe v-if="extJSON.video && ['bilibili', 'youtube'].includes(extJSON.video.type) && extJSON.video.value" :url="extJSON.video.value"/>
+          <video-preview v-if="extJSON.video && extJSON.video.type === 'online' && extJSON.video.value" :url="extJSON.video.value"/>
         </div>
 
         <div class="text-[#576b95] font-medium dark:text-white text-xs mt-2 mb-1 select-none flex items-center gap-0.5"
